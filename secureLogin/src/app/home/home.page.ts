99f537a809e3fc5dc0e223499a4bfbe63dbcb9a7
alegleason <A01703013@itesm.mx>
2020-11-07 14:09:52 -0600

    Add list of users

--- secureLogin/src/app/home/home.page.ts
+++ secureLogin/src/app/home/home.page.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
-import { ModalController } from '@ionic/angular';
+import { Router } from '@angular/router';
+import { ModalController, Platform, ToastController } from '@ionic/angular';
 import { LoginPage } from '../login/login.page';
+import { StorageService, User } from '../services/storage.service';
 
 @Component({
   selector: 'app-home',
@@ -9,12 +11,27 @@ import { LoginPage } from '../login/login.page';
 })
 export class HomePage {
 
-  /* After 30 seconds of inactivity, I want to lock it again! */
+  users: User[] = [];
+
+  constructor(private modalCtrl: ModalController, private router: Router, private storageService: StorageService, private plt: Platform, private toastController: ToastController) {
+    this.plt.ready().then(() => {
+      this.loadUsers();
+    });
+   }
+
+   loadUsers(){
+    this.storageService.getUsers().then(users => {
+      this.users = users;
+    });
+  }
+
+
+  /* After 30 seconds of inactivity, I want to lock it again!
   constructor(private modalCtrl: ModalController) {
     setTimeout(() => {
       this.lockApp();
     }, 30000);
-  }
+  }*/
 
   async lockApp() {
     const modal = await this.modalCtrl.create({
